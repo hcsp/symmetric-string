f5257aaf0f5206d9e987bc9f028260bba0c424b9
Bo Zhang <bo@gradle.com>
2019-06-08 19:48:39 +0800

    Update repo

---
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -0,0 +1,19 @@
+package com.github.hcsp.controlflow;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    public void test() {
+        int n = new java.util.Random().nextInt(50) + 50;
+        String str =
+                IntStream.range(n, n + 10)
+                        .mapToObj(i -> String.format("%c", i))
+                        .collect(Collectors.joining(""));
+        assertTrue(Main.isSymmetric(str + new StringBuilder(str).reverse().toString()));
+        assertFalse(Main.isSymmetric(str + new StringBuilder(str).reverse().toString() + "111"));
+    }
+}
