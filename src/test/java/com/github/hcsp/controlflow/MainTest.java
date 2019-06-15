6f19fc3f92f9183f1f6acc10c64eba85520209f0
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:08 +0800

    Update repo

--- src/test/java/com/github/hcsp/controlflow/MainTest.java
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -1,19 +1,19 @@
 package com.github.hcsp.controlflow;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import java.util.Random;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+public class MainTest {
     @Test
     public void test() {
-        int n = new java.util.Random().nextInt(50) + 50;
+        int n = new Random().nextInt(50) + 50;
         String str =
-                java.util.stream.IntStream.range(n, n + 10)
+                stream.IntStream.range(n, n + 10)
                         .mapToObj(i -> String.format("%c", i))
-                        .collect(java.util.stream.Collectors.joining(""));
-        assertTrue(Main.isSymmetric(str + new StringBuilder(str).reverse().toString()));
-        assertFalse(Main.isSymmetric(str + new StringBuilder(str).reverse().toString() + "111"));
+                        .collect(stream.Collectors.joining(""));
+        Assertions.assertTrue(Main.isSymmetric(str + new StringBuilder(str).reverse().toString()));
+        Assertions.assertFalse(
+                Main.isSymmetric(str + new StringBuilder(str).reverse().toString() + "111"));
     }
 }
