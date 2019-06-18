3a388ecdb8e763611ab6d816216c4fe442bd41bf
lis <liyu.italy@hotmail.com>
2019-06-18 15:07:39 +0800

    check symmetry in string

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -7,12 +7,23 @@ public class Main {
     }
 
     /**
-     * 判断一个字符串是否是对成字符串。一个字符串对称意味着它和它的逆序相同。
+     * 判断一个字符串是否是对称字符串。一个字符串对称意味着它和它的逆序相同。
      *
      * <p>例如，"12321"和"上海自来水来自海上"是对称字符串 "1234"不是对称字符串
      *
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        int count = (str.length() - 1) / 2;
+
+        for (int i = 0;
+             i <= count;
+             i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
