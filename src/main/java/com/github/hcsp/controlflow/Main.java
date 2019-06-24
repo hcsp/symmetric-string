e1e8fbb26c6489c0ebc6ffa9db071c33c76a97b0
Rokixy <zzubillzzu@126.com>
2019-06-24 11:36:48 +0800

    symmetric string

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.controlflow;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(isSymmetric("1234"));
@@ -14,5 +16,16 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        char[] str1 = str.toCharArray();
+        revertCharArray(str1);
+        return Arrays.equals(str.toCharArray(), str1);//这里感觉有点绕弯子了，不知道怎么才能做得更好一些。。。
+    }
+    public static void revertCharArray(char[] str1){
+        for (int i = 0; i <str1.length/2; i++) {
+            char tmp = str1[i];
+            str1[i] = str1[str1.length - i - 1];
+            str1[str1.length - i - 1] = tmp;
+        }
+    }
 }
