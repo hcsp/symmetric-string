8903f6b329ccbdb22f8ba6ac49955a9d0f6b82e4
catsme <845058547@qq.com>
2019-06-25 15:56:00 +0800

    实现了判断一个字符串是否对称的功能。

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.controlflow;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(isSymmetric("1234"));
@@ -14,5 +16,22 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        ArrayList<String> arr;
+        arr = new ArrayList<>();
+        ArrayList<String> rra;
+        rra = new ArrayList<>();
+        for (int i = 0; i <str.length() ; i++) {
+            arr.add(String.valueOf(str.charAt(i)));
+        }//将str顺序存入一个列表
+         for (int j = 0; j <str.length() ; j++) {
+            rra.add(String.valueOf(str.charAt(str.length()-j-1)));
+        }//将str逆序存入一个列表
+//        for (int k = 0; k <str.length() ; k++) {
+//          if (arr.get(k).equals(rra.get(k))) {
+//              System.out.println("相等");
+//            }
+//        }
+        return arr.equals(rra);
+    }
 }
