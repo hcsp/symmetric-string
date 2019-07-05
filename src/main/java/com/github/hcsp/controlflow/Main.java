349408a85d49f456729f716199c7b327c98de275
zzxx9426 <mrxiao9426@gmail.com>
2019-07-05 21:05:22 +0800

    isSymmetric (#23)
    isSymmetric (#23)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,10 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        String newStr = new String(sb);
+        return newStr.equals(str);
+    }
 }
