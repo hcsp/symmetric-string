09502902ae5d0876351933c3dbd99081de3d64c5
Hana <384073877@qq.com>
2019-07-15 17:09:08 +0800

    判断一个字符串是否是对称字符串

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,20 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        for(int i = 0 ; i<str.length();i++){
+            int length = str.length();
+
+            char begin =str.charAt(i);
+
+            char end = str.charAt(length -1 - i);
+
+            if (begin == end){
+
+                if (i == length -1){
+                    return true;
+                }continue;
+                }
+        }return false;
+    }
 }
