0d3aca494250f69c1df0d854690e72f0a6bfd16f
Mandy Tao <taoxiumin2016@gmail.com>
2019-08-08 21:51:38 +0800

    symmetric-string

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,15 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        int length = str.length();
+
+            for (int i = 0, j = length-1; i< length /2 && j > length /2; i++, j--) {
+                if (str.charAt(i) != str.charAt(j)) {
+                    return false;
+                }
+            }
+            return true;
+
+    }
 }
