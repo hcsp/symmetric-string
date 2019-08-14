b7a841eced6ebbda02333030d31af37a4f4c9a12
YakumoAoi <416910533@qq.com>
2019-08-14 17:06:01 +0900

    compare_to_first_and_last_letter (#45)
    compare_to_first_and_last_letter (#45)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,15 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        int strLen=str.length();
+        for(int i=0;i<strLen/2;i++){
+            char before=str.charAt(i);
+            char after= str.charAt(strLen-i-1);
+            if(before!=after) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
