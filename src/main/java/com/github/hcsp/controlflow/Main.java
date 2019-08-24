0fa89117e304ca916056bd404ec0847f1880f3fb
杨汉杰 <36122057+freshmanyang@users.noreply.github.com>
2019-08-24 19:23:24 -0400

    判断对称字符串 (#49)
    判断对称字符串 (#49)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,12 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        for (int i = 0, j = str.length() - 1; i <= j; i++, j--) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
