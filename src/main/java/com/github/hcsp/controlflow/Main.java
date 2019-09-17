94b2ab70791e2535e24731a33ae167cdea7091ff
RookieZ <guilongzhongwei@hotmail.com>
2019-09-17 19:30:25 +0800

    判断是否为对称字符串 (#58)
    判断是否为对称字符串 (#58)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,13 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        int end = str.length() / 2;
+        for (int i = 0; i <= end; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
