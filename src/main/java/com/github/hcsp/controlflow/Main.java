34c02c506fa54fe0468d9f05825bdd6eb90a5435
ws1603 <18388866683@163.com>
2019-10-11 16:15:07 +0800

    判断字符串是否对称

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -2,6 +2,7 @@ package com.github.hcsp.controlflow;
 
 public class Main {
     public static void main(String[] args) {
+        isSymmetric("1234");
         System.out.println(isSymmetric("1234"));
         System.out.println(isSymmetric("1234321"));
     }
@@ -14,5 +15,15 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        char []ch=str.toCharArray();
+        boolean flag=true;
+        for (int i = 0; i < (str.length()+1)/2; i++) {
+            if(ch[i]!=ch[str.length()-i-1]) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
 }
