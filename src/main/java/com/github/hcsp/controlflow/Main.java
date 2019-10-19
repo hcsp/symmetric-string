66d80f055e0f947d4c958f7d2ca0cfb106b3567a
baixc <969552017@qq.com>
2019-10-19 14:43:50 +0800

    判断对称字符串

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,11 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.length() - 1; i>=0; i--) {
+            sb.append(str.charAt(i));
+        }
+        return sb.toString().equals(str);
+    }
 }
