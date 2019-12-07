769cf2c931e869d5604f77704d40230b48093496
sunpeng526 <215860865@qq.com>
2019-12-07 20:28:58 +0800

    Java的控制流练习：判断一个字符串是否是对称字符串

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,10 +9,24 @@ public class Main {
     /**
      * 判断一个字符串是否是对称字符串。一个字符串对称意味着它和它的逆序相同。
      *
-     * <p>例如，"12321"和"上海自来水来自海上"是对称字符串 "1234"不是对称字符串
+     * <p>例如，"12321"和"上海自来水d来自海上"是对称字符串 "1234"不是对称字符串
      *
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        boolean isTrue = true;
+        for (int i = str.length()-1; i >=0; i--) {
+            for (int j = 0; j <str.length() ; j++) {
+                if(i + j == str.length()-1){
+                    char charFromStrEnd = str.charAt(i);
+                    char charFromStrStart = str.charAt(j);
+                    if(charFromStrEnd != charFromStrStart){
+                        isTrue = false;
+                    }
+                }
+            }
+        }
+        return isTrue;
+    }
 }
