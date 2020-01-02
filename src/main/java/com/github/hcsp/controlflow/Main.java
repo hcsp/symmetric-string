7b6a832e0528f4021650c3c4e43a0ac5ef3114bb
Bing Qin <qinbing08@gmail.com>
2020-01-02 18:50:05 +0800

    判断一个字符串是否是对称字符串 (#115)
    判断一个字符串是否是对称字符串 (#115)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,16 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            char star = str.charAt(i);
+            char end = str.charAt(length - 1);
+            if (star != end) {
+                return false;
+            }
+            length--;
+        }
+        return true;
+    }
 }
