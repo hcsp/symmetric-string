c330f005294281444c5b4c1134fad75aaf34dfea
dygalo <59307187+dygalo@users.noreply.github.com>
2020-01-14 00:32:03 +0800

    Dygalo (#125)
    Dygalo (#125)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,8 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        String reverse = new StringBuffer(str).reverse().toString();
+        return str.equals(reverse);
+    }
 }
