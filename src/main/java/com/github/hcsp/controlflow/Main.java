d735dce369b51bd274d58b6c316856ffadaf29b3
mysteryven <33973865+mysteryven@users.noreply.github.com>
2020-01-15 15:12:29 +0800

    计算字符串对称 (#126)
    计算字符串对称 (#126)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,22 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while(true) {
+            if (i >= j) {
+                break;
+            }
+
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
 }
