842eb18530a2140bcc35d7bfdb7d397cef5c94dd
bgzsqqwd123 <a15146980388@163.com>
2020-01-21 18:20:52 +0800

    判断字符串是否对称

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,12 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        StringBuffer str1 = new StringBuffer(str);
+        str1.reverse();
+        if (str.contentEquals(str1)) {
+            return true;
+        }
+        return false;
+    }
 }
