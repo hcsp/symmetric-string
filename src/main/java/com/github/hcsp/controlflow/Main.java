296bf9255092a3fd814bc0033245639eb79539e4
pathjh <1639925487@qq.com>
2020-02-27 14:35:36 +0800

    判断回文字符串 (#145)
    判断回文字符串 (#145)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,17 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        char[] res=str.toCharArray();
+        int left=0,right=res.length-1;
+        while(left<right){
+            if(res[left]==res[right]){
+                left++;
+                right--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
 }
