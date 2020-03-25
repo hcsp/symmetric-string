6328aab36de778829162791ac49dd143167e91f2
7952126 <15521608275@163.com>
2020-03-25 22:37:23 +0800

    string

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.controlflow;
 
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(isSymmetric("1234"));
@@ -14,5 +16,17 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+
+        char a[]=str.toCharArray();
+        StringBuilder reverse = new StringBuilder();
+        for (int i = a.length - 1; i >= 0; i--)
+            reverse.append(a[i]);
+
+        return Objects.equals(reverse.toString(), str);
+
 }
+
+
+}
+
