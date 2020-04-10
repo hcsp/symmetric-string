b0bdc068838f34022472c24b32b76881d2464dab
YUCHEN LIU <181484908@QQ.COM>
2020-04-10 21:37:55 +0800

    work: symmetric-string (#173)
    work: symmetric-string (#173)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,12 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        StringBuilder reverse = new StringBuilder();
+        char[] toCharArr = str.toCharArray();
+        for (int i = toCharArr.length - 1; i >= 0 ; i--) {
+            reverse.append(toCharArr[i]);
+        }
+        return str.equals(reverse.toString());
+    }
 }
