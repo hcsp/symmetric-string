f813a174eb055da68e256e166e74244df09a5c0e
Wong <19876688427>
2020-04-27 09:34:05 +0800

    应该是这样子

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,13 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        char[] c=str.toCharArray();
+        for (int i = 0; i < str.length()/2; i++) {
+            if (c[i] != c[str.length()-i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
 }
