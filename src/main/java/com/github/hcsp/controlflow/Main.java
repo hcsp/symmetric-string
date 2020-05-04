586d0b72086869c2d5099930dfc57a4b57dfe286
DWB-Frank <57584830+DWB-Frank@users.noreply.github.com>
2020-05-05 02:20:25 +0900

    my commit (#181)
    my commit (#181)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,13 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        int midIdx = str.length() / 2;
+        int lastIdx = str.length() - 1;
+        for (int i = 0; i < midIdx; i++) {
+            if (str.charAt(i) != str.charAt(lastIdx - i)) return false;
+        }
+
+        return true;
+    }
 }
