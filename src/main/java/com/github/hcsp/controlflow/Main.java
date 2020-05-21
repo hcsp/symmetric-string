bd7ab9a1a687441ce67d183e1489230a48e59e1d
kongmu373 <kongmu1235@gmail.com>
2020-05-21 12:40:11 +0800

    修改length 为 length /2 .

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static boolean isSymmetric(String str) {
         char[] chars = str.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
+        for (int i = 0; i < chars.length / 2; i++) {
             if (chars[i] != chars[chars.length - 1 - i]) {
                 return false;
             }
