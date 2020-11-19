1652f79ad68f189b387964ddc85be8a78f1d177d
qipengWei <webqipeng@163.com>
2020-11-19 23:14:53 +0800

    判断字符串

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,9 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        String start = str.split("")[0];
+        String end = str.split("")[str.split("").length - 1];
+        return start.equals(end);
+    }
 }
