44678e578f161c55951d68ce0b015aa7358d00be
weiranyi <168578203@qq.com>
2020-11-29 17:24:28 +0800

    借用if分支判断奇偶，再进行str遍历操作

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,34 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        //str为偶数的情况
+        if (str.length() % 2 == 0) {
+            int j = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == str.charAt(str.length()-1)) {
+                    j++;
+                }
+            }
+            if (j==str.length()/2){
+                return true;
+            }else {
+                return false;
+            }
+        //str为奇数的情况
+        } else {
+            int j = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == str.charAt(str.length()-1)) {
+                    j++;
+                }
+            }
+            if (j==str.length()/2-1){
+                return true;
+            }else {
+                return false;
+            }
+        }
+
+    }
 }
