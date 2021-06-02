b68e992b349bf3081f46a5ffb54acbe5256a7529
Bowen Wu <bwwu@iflytek.com>
2021-06-02 10:38:02 +0800

    feat: use fori judge everyone

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,8 +15,13 @@ public class Main {
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
     public static boolean isSymmetric(String str) {
-        StringBuilder newStr = new StringBuilder(str);
-        newStr.reverse();
-        return newStr.toString().equals(str);
+        for (int i = 0; i < str.length() / 2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - 1 - i);
+            if (start != end) {
+                return false;
+            }
+        }
+        return true;
     }
 }
