720f213fb4d5376c0bb8ebcd4499927ec68b9ea7
Ocultaz <syuuhiyou@yahoo.co.jp>
2021-06-03 15:56:44 +0900

    CheckSym

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,15 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        char[] ch = str.toCharArray();
+        for (int i = 0; i < ch.length/2; i++) {
+            if (ch[i] != ch[ch.length -1 - i]) {
+                return false;
+                }
+        }
+        return true;
+
+}
+
 }
