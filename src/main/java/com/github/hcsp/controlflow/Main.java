bf498a5a9b1af6d7bc2b8dfb2b2fd150128db24e
ROG <1049378767@qq.com>
2021-09-18 10:52:42 +0800

    判断一个字符串是否是对称字符串

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,22 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+
+        char arr[] = str.toCharArray();
+
+        for(int i = 0,j = arr.length-1;i < j;i++,j--){
+
+            if(arr[i]!=arr[j]){
+
+                return false;
+
+            }
+
+        }
+
+        return true;
+
+
+    }
 }
