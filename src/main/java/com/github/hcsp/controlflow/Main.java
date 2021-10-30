320a3fddec4372cb5360bb637d3bf36586fdff3e
小虾米 <freebufer.wang@gmail.com>
2021-10-31 04:52:34 +0800

    Symmetric string test (#251)
    Symmetric string test (#251)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,13 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        char[] chs = str.toCharArray();
+        for (int i = 0; i <= chs.length / 2; i++) {
+            if (chs[i] != chs[chs.length-i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
