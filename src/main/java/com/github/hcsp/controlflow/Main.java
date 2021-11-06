ebd269408297fb23e1ef6962bd84833a202503ba
lee <lindamoxiaobei@163.com>
2021-11-06 22:51:46 +0000

    打印回文

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,16 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        char[] charArray = str.toCharArray();
+        for (int i = 0,j = str.length()-1; i < str.length(); i++,j--) {
+            if (charArray[i] != charArray[j]) {
+                break;
+            }
+            return true;
+        }
+        return false;
+
+
+    }
 }
