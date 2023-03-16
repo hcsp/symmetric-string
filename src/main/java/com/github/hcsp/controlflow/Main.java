34e6d2183d4ebbd67bc3d919f2b4c37f6a9ee902
AndyDrew163 <120002194+AndyDrew163@users.noreply.github.com>
2023-03-17 04:45:57 +0800

    判断一个字符串是否是对称字符串完成 (#282)
    判断一个字符串是否是对称字符串完成 (#282)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -2,7 +2,7 @@ package com.github.hcsp.controlflow;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isSymmetric("1234"));
+        System.out.println(isSymmetric("123422"));
         System.out.println(isSymmetric("1234321"));
     }
 
@@ -14,5 +14,12 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            stringBuilder.append(str.charAt(i));
+        }
+        String text=stringBuilder.toString();
+        return text.equals(str);
+    }
 }
