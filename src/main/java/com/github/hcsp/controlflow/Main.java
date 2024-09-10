933329712f6fa5382406e73146f6bf76270fc3b5
GJV123456 <161851858+GJV123456@users.noreply.github.com>
2024-09-10 11:22:31 +0800

    对称字符串 (#288)
    对称字符串 (#288)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,14 @@ public class Main {
      * @param str 给定的字符串
      * @return 若给定的字符串是对称的，返回true，否则返回false
      */
-    public static boolean isSymmetric(String str) {}
+    public static boolean isSymmetric(String str) {
+        int length = str.length()-1;
+        int a = 0;
+        while (str.charAt(a) == str.charAt(length) && a <= length) {
+            a++;
+            length--;
+        }
+        return a >= length;
+
+    }
 }
